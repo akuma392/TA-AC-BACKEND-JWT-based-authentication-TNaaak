436d148d1e2d2a9e6c5e7cb947680de01317e0bd
Abhishek Kumar <abhivision3@gmail.com>
2021-04-12 22:00:12 +0530

    refactor

--- block-BNaaen/conduit/routes/profile.js
+++ block-BNaaen/conduit/routes/profile.js
@@ -23,4 +23,53 @@ router.get('/:id', auth.verifyToken, async (req, res, next) => {
     next(error);
   }
 });
+
+router.post('/:username/follow', auth.verifyToken, async (req, res, next) => {
+  var username = req.params.username;
+  try {
+    let loggedinUserId = req.user.userId;
+    var loggedinUser = await User.findOne({ _id: loggedinUserId });
+    var userToBeUpdated = await User.findOne({ username });
+    var followersList = userToBeUpdated.followers;
+    if (!followersList.includes(loggedinUserId)) {
+      userToBeUpdated.followers.push(loggedinUserId);
+      userToBeUpdated.following = true;
+      userToBeUpdated.save();
+    }
+    if (!loggedinUser.followings.includes(userToBeUpdated._id)) {
+      loggedinUser.followings.push(userToBeUpdated._id);
+      loggedinUser.save();
+    }
+    res.json({ profile: userToBeUpdated });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete(
+  '/:username/unfollow',
+  auth.verifyToken,
+  async (req, res, next) => {
+    var username = req.params.username;
+    try {
+      var loggedinUserId = req.user.userId;
+      var loggedinUser = await User.findOne({ _id: loggedinUserId });
+      var userToBeUpdated = await User.findOne({ username });
+
+      var followersList = userToBeUpdated.followers;
+      if (followersList.includes(req.user.userId)) {
+        userToBeUpdated.followers.pull(req.user.userId);
+        userToBeUpdated.following = false;
+        userToBeUpdated.save();
+      }
+      if (loggedinUser.followings.includes(userToBeUpdated._id)) {
+        loggedinUser.followings.pull(userToBeUpdated._id);
+        loggedinUser.save();
+      }
+      res.json({ profile: userToBeUpdated });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 module.exports = router;
